8be9c9864ce59ad5d3f9a86bc2c53f8570076700
Adrior11 <adrian.schneider11@gmail.com>
2025-10-27 11:37:19 +0100

    chore: remove tailwind-tools from nvim

--- nvim/lua/config/plugins/tailwind-tools.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"luckasRanarison/tailwind-tools.nvim",
-	name = "tailwind-tools",
-	build = ":UpdateRemotePlugins",
-	ft = { "html", "css", "javascript", "typescript", "javascriptreact", "typescriptreact", "tsx", "jsx" },
-	dependencies = { "nvim-treesitter/nvim-treesitter", "neovim/nvim-lspconfig" },
-	opts = {},
-}
