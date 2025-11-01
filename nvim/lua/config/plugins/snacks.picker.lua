04867904e8b5e3fa9bfc367d9cebde0296852635
Adrior11 <adrian.schneider11@gmail.com>
2025-11-01 12:00:04 +0100

    chore: updates

--- nvim/lua/config/plugins/snacks.picker.lua
+++ nvim/lua/config/plugins/snacks.picker.lua
@@ -70,7 +70,8 @@ return {
 				},
 				file = {
 					filename_first = true,
-					truncate = 1000,
+					truncate = "left",
+					min_width = 40,
 					filename_only = false,
 					icon_width = 2,
 					git_status_hl = true,
